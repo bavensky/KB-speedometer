8273f9b147a3e96785ffac03c8520fa60dbec46f
aphirak sang-ngenchai <apiruk_sang-ngenchai@hotmail.com>
2019-01-19 12:32:09 +0700

    edit i2c code

--- KB-speedometer/KB-speedometer.ino
+++ KB-speedometer/KB-speedometer.ino
@@ -1,29 +1,33 @@
 /*\
- * KidBright Speed Detector 
- * 
- * -------------
- * Pin connnect
- * ---------------------------------------------------------------- 
- * KidBright    VNCL4200(KB Chain Proximiter)  VL53L0X(GYVL53L0XV2)
- * ----------------------------------------------------------------
- * 5V                       5V                         5V
- * 3V3                      3V3                        3V3
- * SDA0 (21)                SDA                        SDA
- * SCL0 (22)                SCL                        SCL
- * GND                      GND                        GND
- * ----------------------------------------------------------------
- * 
- * ref : http://electron6.phys.utk.edu/101/CH1/speed_and_velocity.htm
- * 
- */
- 
+   KidBright Speed Detector
+
+   -------------
+   Pin connnect
+   ----------------------------------------------------------------
+   KidBright    VNCL4200(KB Chain Proximiter)  VL53L0X(GYVL53L0XV2)
+   ----------------------------------------------------------------
+   5V                       5V                         5V
+   3V3                      3V3                        3V3
+   SDA0 (21)                SDA                        SDA
+   SCL0 (22)                SCL                        SCL
+   GND                      GND                        GND
+   ----------------------------------------------------------------
+
+   ref : http://electron6.phys.utk.edu/101/CH1/speed_and_velocity.htm
+
+*/
+
 #include <Wire.h>
 #include "Adafruit_VL53L0X.h"
+#include <Adafruit_GFX.h>
+#include "Adafruit_LEDBackpack.h"
 
+Adafruit_8x16minimatrix matrix = Adafruit_8x16minimatrix();
 Adafruit_VL53L0X lox = Adafruit_VL53L0X();
 
-#define SENSOR_ADDR 0x51 //vcnl4200
+//TwoWire Wire1;/
 
+#define SENSOR_ADDR 0x51 //vcnl4200
 #define distance_m 0.3 // 30 cm to meter (30/100 = 0.3)
 
 boolean GotFirstSensor = false, GotSecondSensor = false;
@@ -35,35 +39,53 @@ double velocity;
 
 void write_register(int device_addr, int register_num, int low_byte, int high_byte)
 {
-  Wire.beginTransmission(device_addr);
-  Wire.write(register_num);
-  Wire.write(low_byte);
-  Wire.write(high_byte);
-  Wire.endTransmission(false);
+  Wire1.beginTransmission(device_addr);
+  Wire1.write(register_num);
+  Wire1.write(low_byte);
+  Wire1.write(high_byte);
+  Wire1.endTransmission(false);
 }
 
 void read_first_sensor() {
   int reg = 0x8;
-  Wire.beginTransmission(SENSOR_ADDR);
-  Wire.write(reg);
-  Wire.endTransmission(false);
+  Wire1.beginTransmission(SENSOR_ADDR);
+  Wire1.write(reg);
+  Wire1.endTransmission(false);
   byte data[2] = {0, 0};
-  Wire.requestFrom(SENSOR_ADDR, 2);
-  data[0] = Wire.read();
-  data[1] = Wire.read();
+  Wire1.requestFrom(SENSOR_ADDR, 2);
+  data[0] = Wire1.read();
+  data[1] = Wire1.read();
   first_sensor_distance = int(data[1]) * 256 + int(data[0]);
+  //  Serial.print("first_sensor_distance = ");/
+  //  Serial.println(first_sensor_distance);/
 }
 
 void read_secound_sensor() {
   VL53L0X_RangingMeasurementData_t measure;
   lox.rangingTest(&measure, false);
   secound_sensor_distance = measure.RangeMilliMeter;
+  //  Serial.print("secound_sensor_distance = ");/
+  //  Serial.println(secound_sensor_distance);/
 }
 
 void setup() {
 
   Serial.begin(115200);
   Wire.begin(21, 22);
+  Wire1.begin(4, 5);
+  matrix.begin(0x70);
+  matrix.setRotation(1);
+  matrix.setTextSize(1);
+  matrix.setTextColor(LED_ON);
+
+  for (int8_t x = 0; x >= -128; x--) {
+    matrix.clear();
+    matrix.setCursor(x, 0);
+    matrix.print("KidBright Speedometer");
+    matrix.writeDisplay();
+    delay(100);
+  }
+  matrix.clear();
 
   // init vcnl4200
   write_register(SENSOR_ADDR, 0x3, B00011010, B00001000);
@@ -80,13 +102,13 @@ void loop() {
   read_first_sensor();
   read_secound_sensor();
 
-  if (first_sensor_distance > 100) {
+  if (first_sensor_distance > 200 && GotSecondSensor == false) {
     //    t1_millis = millis();
     t1_millis = micros();
     GotFirstSensor = true;
   }
 
-  if (secound_sensor_distance < 150) {
+  if (secound_sensor_distance < 150 && GotFirstSensor == true) {
     //    t2_millis = millis();
     t2_millis = micros();
     GotSecondSensor = true;
@@ -102,7 +124,7 @@ void loop() {
     time_ms = t2_millis - t1_millis;
     Serial.print("t micro ");
     Serial.println(time_ms);
-    
+
     time_ms = time_ms / 1000;  // convert microsecond to millisecond
     Serial.print("time_ms ");
     Serial.println(time_ms);
@@ -120,15 +142,31 @@ void loop() {
     Serial.println(KMH);
 
     KMH = KMH / 1000; // meters per Km
+
     Serial.print("speed = ");
     Serial.print(KMH);
     Serial.print(" km/h");
     Serial.print("\n");
-    delay(3000);
+
+    for (int8_t x = 0; x >= -100; x--) {
+      matrix.clear();
+      matrix.setCursor(x, 0);
+      matrix.print("Speed = ");
+      matrix.print(KMH);
+      matrix.print(" km/h");
+      matrix.writeDisplay();
+      delay(100);
+    }
+    matrix.clear();
+
     // reset to detector speed again
+
     GotFirstSensor = false;
     GotSecondSensor = false;
   } else {
+    matrix.setCursor(0, 0);
+    matrix.print("  ");
+    matrix.writeDisplay();
     Serial.print("1st = ");
     Serial.print(first_sensor_distance);
     Serial.print(" 2st = ");
